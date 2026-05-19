da15d769f53c94b1baf5db20ee1b4f9fac8890ac
Syed-Talmin <saiyedsufyan7@gmail.com>
2026-05-19 17:50:23 +0530

    feat: add robots.txt configuration for SEO optimization

---
+++ src/app/robot.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://subscription.wensforce.com/sitemap.xml',
+  }
+}
