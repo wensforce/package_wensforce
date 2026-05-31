953c948947c95a8d4dd163ff446b07b52ec8e47a
Syed-Talmin <saiyedsufyan7@gmail.com>
2026-05-31 14:40:31 +0530

    fix: add Microsoft Clarity tracking script to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -96,6 +96,16 @@ export default function RootLayout({ children }) {
           }}
         />
         {/* End Google Tag Manager */}
+
+        {/* Microsoft Clarity */}
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","wzmm5dme82");`,
+          }}
+        />
+        {/* End Microsoft Clarity */}
       </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <JsonLd
